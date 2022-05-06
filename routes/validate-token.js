6ea1c1ea4ecff921b4cfe314b72a1087e6ab3ba7
konobabuno <konobabuno@gmail.com>
2022-05-06 15:01:00 -0500

    Cambios

---
+++ routes/validate-token.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+const verifyToken = (req, res, next) => {
+    const token = req.header('auth-token')
+    if(!token){
+        return res.status(400).json({
+            error: 'Acceso denegado'
+        })
+    }
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+        req.user = verified
+        next()
+    } catch(error){
+        res.status(400).json({
+            error: 'Token invalidos'
+        })
+    }
+
+}
+
+module.exports = verifyToken
